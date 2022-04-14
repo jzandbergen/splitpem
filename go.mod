4bfa82a0dfa55fca72a1ab2dabebbdfbdfa3681b
Justin Zandbergen <justin.zandbergen@topicus.nl>
2022-04-14 10:25:41 +0200

    chore(main): It works!

---
+++ go.mod
@@ -0,0 +1,3 @@
+module git.sr.ht/~goestin/splitpem
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var file = "clientchain.pem"
+
+func main() {
+
+	// Read the file into "bytes".
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// In the following loop we need access to "bytes", so we define
+	// "block" in the outer scope as well.
+	var block *pem.Block
+
+	for {
+		// Check if there are more bytes left that we need to process.
+		if len(bytes) == 0 {
+			log.Println("Done...")
+			break
+		}
+
+		// Decode pem block, and return bytes left to work trough.
+		block, bytes = pem.Decode(bytes)
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		outputFilename := strings.ToLower(
+			strings.ReplaceAll(cert.Subject.CommonName, " ", "_"),
+		) + ".pem"
+
+		f, err := os.Create("output/" + outputFilename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		log.Printf("Writing: %v", f.Name())
+		pem.Encode(f, block)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
